5e19a10a1e92c0947aa876f202605c7c094b3364
nishmaha176 <nishmaha176@SDS-L-F81HC63>
2024-11-08 11:25:31 -0500

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class johnPorksMansion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(johnPorksMansion));
+            this.option1Button = new System.Windows.Forms.Button();
+            this.option3Button = new System.Windows.Forms.Button();
+            this.option2Button = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // option1Button
+            // 
+            this.option1Button.Location = new System.Drawing.Point(24, 294);
+            this.option1Button.Name = "option1Button";
+            this.option1Button.Size = new System.Drawing.Size(75, 23);
+            this.option1Button.TabIndex = 0;
+            this.option1Button.UseVisualStyleBackColor = true;
+            this.option1Button.Click += new System.EventHandler(this.option1Button_Click);
+            // 
+            // option3Button
+            // 
+            this.option3Button.Location = new System.Drawing.Point(24, 391);
+            this.option3Button.Name = "option3Button";
+            this.option3Button.Size = new System.Drawing.Size(75, 23);
+            this.option3Button.TabIndex = 1;
+            this.option3Button.UseVisualStyleBackColor = true;
+            // 
+            // option2Button
+            // 
+            this.option2Button.Location = new System.Drawing.Point(24, 341);
+            this.option2Button.Name = "option2Button";
+            this.option2Button.Size = new System.Drawing.Size(75, 23);
+            this.option2Button.TabIndex = 2;
+            this.option2Button.UseVisualStyleBackColor = true;
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Location = new System.Drawing.Point(212, 24);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(35, 13);
+            this.outputLabel.TabIndex = 3;
+            this.outputLabel.Text = "label1";
+            // 
+            // johnPorksMansion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(464, 450);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.option2Button);
+            this.Controls.Add(this.option3Button);
+            this.Controls.Add(this.option1Button);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "johnPorksMansion";
+            this.Text = "John Pork\'s Mansion";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button option1Button;
+        private System.Windows.Forms.Button option3Button;
+        private System.Windows.Forms.Button option2Button;
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace WindowsFormsApp1
+{
+    public partial class johnPorksMansion : Form
+    {
+        int page = 1;
+        public johnPorksMansion()
+        {
+            InitializeComponent();
+            outputLabel.Text = "You are trapped in the basement of John Por's Mansion. How will you escape?";
+            option1Button.Text = "Window";
+            option2Button.Text = "Vent System";
+
+        }
+
+        private void option1Button_Click(object sender, EventArgs e)
+        {
+            if (page == 1)
+            {
+                page = 3;
+            }
+            else if (page == 2)
+            {
+                page = 
+            }
+        }
+    }
+}
